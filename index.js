9cbea0c95704085932e8cf9e776f6452cb510e9f
JuanMa <mitild82@gmail.com>
2023-01-09 00:20:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ document.addEventListener('load', fetchColors())
 // GET NEW SCHEME ON BUTTON CLICK
 document.getElementById('get-color-btn').addEventListener('click', fetchColors)
 
-c
 document.addEventListener('click', e => {
   if(e.target.dataset.code){
     navigator.clipboard.writeText(e.target.dataset.code)
@@ -23,8 +22,6 @@ document.addEventListener('mouseover', e => {
     e.target.setAttribute("data-tooltip", "Copy to clipboard")
   }
 })
-
-
 
 // FETCH COLORS FROM API
 function fetchColors() {
